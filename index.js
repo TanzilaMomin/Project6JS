b70dcfc3f966498ac6c507140687ca1881db5fc9
TanzilaMomin <147149600+TanzilaMomin@users.noreply.github.com>
2023-11-01 14:06:57 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,20 @@
+function handleHow(){
+    alert("I am Fine,Thanks you!");
+    document.getElementById("bg").style.backgroundColor="Green";
+}
+function handleMorning(){
+    alert("Good Morning!!!");
+    document.getElementById("bg").style.backgroundColor="yellow";
+}
+function handleAfternoon(){
+    alert("Good Afternoon!!!!");
+    document.getElementById("bg").style.backgroundColor="silver";
+}
+function handleEvening(){
+    alert("Good Evening!");
+    document.getElementById("bg").style.backgroundColor="coral";
+}
+function handleNight(){
+    alert("Good Night!!!");
+    document.getElementById("bg").style.backgroundColor="grey";
+}
